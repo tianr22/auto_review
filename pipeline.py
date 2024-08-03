3d33e83ce1ba1dbf901d669ad5ef0dc4372305b3
Atomie CHEN <atomic_cwh@163.com>
2024-08-03 18:17:44 +0800

    finish pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,69 +1,114 @@
 import pandas as pd
 import os
 import json
-import numpy as np
 import ast
+class Info:
+    def __init__(self,**kwargs):
+        self.student_fact_status = kwargs.get('student_fact_status', {} )
+        self.operation_right = kwargs.get('operation_right', {} )
+        self.operation_wrong = kwargs.get('operation_wrong', {} )
+        self.student_num = kwargs.get('student_num', 0 )
+        self.fact_info = kwargs.get('fact_info', [] )
+        self.student_op_num = kwargs.get('student_op_num', [] )
+        self.student_fact_num = kwargs.get('student_fact_num', [] )
+        self.pair_info = kwargs.get('pair_info', [] )
+        self.student_status = kwargs.get('student_status', [] )
+        self.operation_status = kwargs.get('operation_status', {} )
 
-def select_student():
-    ## 选择下一个进行批改的学生
-    pass
+    def update(self, **kwargs):
+        self.student_fact_status = kwargs.get('student_fact_status', self.student_fact_status)
+        self.operation_right = kwargs.get('operation_right', self.operation_right)
+        self.operation_wrong = kwargs.get('operation_wrong', self.operation_wrong)
+        self.student_num = kwargs.get('student_num', self.student_num)
+        self.fact_info = kwargs.get('fact_info', self.fact_info)
+        self.student_op_num = kwargs.get('student_op_num', self.student_op_num)
+        self.student_fact_num = kwargs.get('student_fact_num', self.student_fact_num)
+        self.pair_info = kwargs.get('pair_info', self.pair_info)
+        self.student_status = kwargs.get('student_status', self.student_status)
+        self.operation_status = kwargs.get('operation_status', self.operation_status)
 
+    def save_as_json(self, path):
+        with open(path, 'w') as f:
+            json.dump(self.__dict__, f, indent=4)
+
+    @classmethod
+    def load_from_json(cls, path):
+        with open(path, 'r') as f:
+            dic = json.load(f)
+        obj = cls()
+        obj.__dict__.update(dic)
+        return obj
+
+## DONE
 def get_all_student_json_files(json_dic_path,ignore=False,student_status=None):
     json_files = os.listdir(json_dic_path)
+    json_files=[json_file for json_file in json_files if json_file.endswith(".json") and json_file[0].isdigit()]
     json_files=sorted(json_files)
-    json_files=[json_file for json_file in json_files if json_file[0].endswith(".json") and json_file[0].isdigit()]
     if not ignore:
         return json_files
     if student_status is not None:
         json_files=[json_file for index,json_file in enumerate(json_files) if student_status[index]!='A']
         return json_files
 
+## TODO  
+def select_student():
+    ## 选择下一个进行批改的学生
+    pass
+
+## DONE
 ## 读取judge info
-def read_info(store_path,same_in_df_right_path,from_student_id_list_path):
-    with open(store_path, 'r') as f:
-        store_dict = json.load(f)
+def read_info(store_path,same_in_df_right_path):
+    info = Info.load_from_json(store_path)
     with open(same_in_df_right_path) as f:
         data_same_in = pd.read_csv(f)
-    from_student_id_list = np.load(from_student_id_list_path)
-    return store_dict,data_same_in,from_student_id_list
+    return info,data_same_in
 
 ## 前端发送批改好的学生数据给后端。后端更新学生数据
 def refresh_student_data(review_id,update_data):
     # TODO backend Update student_data
-    ## 更新student_left_id
-    student_left_id=[i for i in student_left_id if i != review_id]
     pass
 
-def refresh_backend_data(json_dic_path,store_path,same_in_df_right_path,from_student_id_list_path):
+## DONE
+## TO BE CHECKED
+def refresh_backend_data(json_dic_path,store_path,same_in_df_right_path):
     # 通过fact更新所有的operation的正误
     # 原则是如果输入都对，但是输出包含错误信息，则op错
     # 如果输出都对，输入包含错误信息，则op错
     # 但是如果输入输出都包含错误，则无法判断，需要老师进行矫正
-    store_dict,data_same_in,from_student_id_list=read_info(store_path,same_in_df_right_path,from_student_id_list_path)
-    fact_info=store_dict['pair_info']
-    operation_right=store_dict['operation_right']
-    operation_wrong=store_dict['operation_wrong']
-    pair_info=store_dict['pair_info']
-    student_op_num=store_dict['student_op_num']
-    num_student=store_dict['student_num']
-    student_status=store_dict['student_status']
-    operation_status=store_dict['operation_status']
+    info,data_same_in=read_info(store_path,same_in_df_right_path)
+    student_fact_status=info.student_fact_status
+    fact_info=info.fact_info
+    operation_right=info.operation_right
+    operation_wrong=info.operation_wrong
+    pair_info=info.pair_info
+    student_op_num=info.student_op_num
+    student_fact_num=info.student_fact_num
+    num_student=info.student_num
+    student_status=info.student_status
+    operation_status=info.operation_status
     json_files=get_all_student_json_files(json_dic_path)
-    student_to_delete=[]
     ## use fact info to refresh op
     for id,status,json_file in zip(enumerate(student_status),json_files):
         ## 如果该学生已被老师批改
         if status == 'A':
             continue
+        ## 更新未经老师批改的学生的状态
         ## 每次都需要重新检验未批完的学生的op
         student_status[id]='C'
         operation_wrong[id]=[]
         operation_right[id]=[]
         operation_status[id]=["C" for _ in range(student_op_num[id])]
+        student_fact_status[id]=['unknown' for _ in range(student_fact_num[id])]
         with open(json_dic_path+json_file, 'r') as file:
             data=json.load(file)
             nodes=data["nodes"]
             op_nodes=[node for node in nodes if node['type']=='operation']
+            fact_nodes=[node for node in nodes if node['type']=='fact']
+            # 更新学生fact状态
+            for index,fact_node in enumerate(fact_nodes):
+                if fact_info[fact_node['factid']-1]!=-1:
+                    student_fact_status[id][index]='known'
+            # 更新op状态
             for index,op_node in enumerate(op_nodes):
                 is_from_correct=True
                 is_to_correct=True
@@ -88,31 +133,34 @@ def refresh_backend_data(json_dic_path,store_path,same_in_df_right_path,from_stu
                         pass
                     elif (is_from_correct and not is_from_uncertain) or (is_to_correct and not is_to_uncertain):
                         operation_wrong[id].append(op_node['id'])
-                        operation_status[id][index]='B'
+                        ## 更新op状态
+                        operation_status[id][index]='B_fact'
                     else: 
                         pass
                 elif is_from_correct and is_to_correct:
                     operation_right[id].append(op_node['id'])
-                    operation_status[id][index]='B'
+                    operation_status[id][index]='B_fact'
                 elif is_from_correct != is_to_correct:
                     operation_wrong[id].append(op_node['id'])
-                    operation_status[id][index]='B'
-                if all(_=='B' for _ in operation_status[id]):
+                    operation_status[id][index]='B_fact'
+                if all(_=='B_fact' for _ in operation_status[id]):
                     student_status[id]='B'
                                              
     # 根据现在的operation的正误，更新pair的正误
     while True:
-        is_need_continue=False
+        ## 更新pair
         for pair_id in range(len(data_same_in)):
             pair_info[pair_id]=-1
             def refresh_pair(from_student_id):
                 consist_ops = data_same_in.loc[pair_id, str(from_student_id + 1)]
-                if consist_ops=="set()":
-                    continue
-                else:
-                    consist_ops = ast.literal_eval(data_same_in.loc[pair_id, str(from_student_id + 1)])
+                is_exist=True
                 is_pair_correct=True
                 is_pair_unknown=False
+                if consist_ops=="set()":
+                    is_exist=False
+                    return is_pair_correct,is_pair_unknown,is_exist
+                else:
+                    consist_ops = ast.literal_eval(data_same_in.loc[pair_id, str(from_student_id + 1)])
                 for opeation_id in consist_ops:
                     if opeation_id not in operation_right[pair_id]:
                         if opeation_id in operation_wrong[pair_id]:
@@ -121,116 +169,131 @@ def refresh_backend_data(json_dic_path,store_path,same_in_df_right_path,from_stu
                             break
                         else:
                             is_pair_unknown=True
-                return is_pair_correct,is_pair_unknown
-            for student_id in range(num_student):
-                is_pair_correct,is_pair_unknown=refresh_pair(student_id)
-                # 只要有能判断pair正确的，这个pair就是正确的
-                if is_pair_correct:
-                    pair_info[pair_id]=1
-                    break
-                elif is_pair_unknown:
-                    pair_info[pair_id]=-1
-                    
+                return is_pair_correct,is_pair_unknown,is_exist
+            if any(is_exist and is_pair_correct and not is_pair_unknown for is_pair_correct,is_pair_unknown,is_exist in [refresh_pair(student_id) for student_id in range(num_student)]):
+                pair_info[pair_id]=1
+            elif all(not is_exist or not is_pair_correct for is_pair_correct,_,is_exist in [refresh_pair(student_id) for student_id in range(num_student)]):
+                pair_info[pair_id]=0
+        ## 完成pair的更新  
+
+        ## 检查是否有op更新  
+        is_need_continue=False
+        for pair_id in range(len(data_same_in)):
             in_data = ast.literal_eval(data_same_in.loc[pair_id,"in"])
             out_data = ast.literal_eval(data_same_in.loc[pair_id,"out"])
-            student_to_delete=[]
-            for index,json_file in zip(student_left_id,json_files):
+            for id,status,json_file in zip(enumerate(student_status),json_files):
+                if status!="C":
+                    continue
                 with open(json_dic_path+json_file,'r') as file:
                     data=json.load(file)
                     nodes=data['nodes']
                     op_nodes=[node for node in nodes if node['type']=='operation']
-                    for op_node in op_nodes:
-                        if op_node['id'] in operation_right[index] or op_node['id'] in operation_wrong[index]:
+                    for index,op_node in enumerate(op_nodes):
+                        if operation_status[id][index]!='C':
                             continue
                         from_fact_set=set([node['factid'] for node in nodes if node['id'] in op_node['from']])
                         to_fact_set=set([node['factid'] for node in nodes if node['id'] in op_node['to']])
                         if from_fact_set==in_data:
                             if pair_info[pair_id] and to_fact_set.issubset(out_data):
                                 operation_right[index].append(op_node['id'])
+                                operation_status[id][index]='B_pair'
                                 is_need_continue=True
                             elif pair_info[pair_id]==0 and out_data.issubset(to_fact_set):
                                 operation_wrong[index].append(op_node['id'])
+                                operation_status[id][index]='B_pair'
                                 is_need_continue=True
-                if len(operation_wrong[index])+len(operation_right[index])==student_op_num[index]:
-                    student_to_delete.append(index)
-            if len(student_to_delete) > 0:
-                student_left_id=[i for i in student_left_id if i not in student_to_delete]
-                json_files=get_all_student_json_files(json_dic_path,student_left_id)
-                if len(student_left_id)==0:
-                    is_need_continue=False
-                    break
+                if all(_!='C' for _ in operation_status[id]):
+                    student_status[id]='B'
         if not is_need_continue:
             break
-    store_dict['operation_right']=operation_right
-    store_dict['operation_wrong']=operation_wrong
-    store_dict['student_left_id']=student_left_id
-    store_dict['pair_info']=pair_info
-    with open(store_path, 'w') as f:
-        json.dump(store_dict, f, indent=4)
+    info.update(student_fact_status=student_fact_status,
+                operation_right=operation_right,
+                operation_wrong=operation_wrong,
+                pair_info=pair_info,
+                student_status=student_status,
+                operation_status=operation_status
+                )
+    info.save_as_json(store_path)
 
+## DONE
 def is_finished(store_path):
-    with open(store_path, 'r') as f:
-        store_dict = json.load(f)
-    student_left_id=store_dict['student_left_id']
-    if len(student_left_id)==0:
+    info = Info.load_from_json(store_path)
+    if all(_!='C' for _ in info.student_status):
         return True
-    else:
-        return False
+    return False
 
+## DONE 
 ## init_auto_judge
 def judge_init(json_dic_path,store_path,fact_list_path,same_in_df_right_path):
     ## 配置相关参数
     with open(fact_list_path, 'r', encoding='utf-8') as file:
         # 读取文件的所有内容
         list_str = file.read()
-        # 将字符串解析为列表
-        fact_list = json.loads(list_str)
-    fact_list_length=len(fact_list)
+        # 计算 "factid" 出现的次数
+        fact_list_length = list_str.count('"factid"')
     # 记录所有 fact 的正误，1代表正确，0代表错误,-1代表未知
     fact_info=[-1 for _ in range(fact_list_length)]
     # 记录每个人当前operation的状态
     operation_status={}
     operation_right={}
     operation_wrong={}
-    json_files=get_all_student_json_files(json_dic_path)
+    student_fact_status={}
     with open(same_in_df_right_path) as f:
         data_same_in = pd.read_csv(f)
     ## 表示pair的正误,-1表示未知
-    pair_info = [-1 for i in range(len(data_same_in))]
+    pair_info = [-1 for _ in range(len(data_same_in))]
+    json_files=get_all_student_json_files(json_dic_path)
     # 学生数量
     num_student=len(json_files)
     # 学生共有两种状态，老师手动批改过的学生为A，自动推断批完的学生为B，其余为C
+    # Update
     student_status=['C' for _ in range(num_student)]
     student_op_num=[]
+    student_fact_num=[]
     for index,json_file in enumerate(json_files):
         operation_right[index]=[]
         operation_wrong[index]=[] 
-        student_data=json.load(json_file)
-        student_nodes=student_data['nodes']
-        student_op_nodes=[op_node for op_node in student_nodes if op_node['type']=='operation']
-        student_op_num.append(len(student_op_nodes))
-        # C表示unknown B代表推断 A代表老师批改
-        operation_status[index]=['C' for _ in range(len(student_op_nodes))]   
-    student_left_id=[i for i in range(num_student)]
-    store_dict={}
-    store_dict['operation_right']=operation_right
-    store_dict['operation_wrong']=operation_wrong
-    store_dict['student_num']=num_student
-    store_dict['fact_info']=fact_info
-    store_dict['student_op_num']=student_op_num
-    store_dict['student_left_id']=student_left_id
-    store_dict['pair_info']=pair_info
-    store_dict['student_status']=student_status
-    store_dict['operation_status']=operation_status
-    with open(store_path, 'w') as f:
-        json.dump(store_dict, f, indent=4)
+        with open(json_dic_path+json_file, 'r') as json_file:
+            student_data=json.load(json_file)
+            student_nodes=student_data['nodes']
+            student_fact_nodes=[fact_node for fact_node in student_nodes if fact_node['type']=='fact']
+            student_op_nodes=[op_node for op_node in student_nodes if op_node['type']=='operation']
+            student_op_num.append(len(student_op_nodes))
+            student_fact_num.append(len(student_fact_nodes))
+            # C表示unknown B代表推断 A代表老师批改
+            # Update: B分为两种 B_fact 表示由fact推断，B_pair 表示由pair推断出来的结果
+            # 优先级：A > B_fact > B_pair > C
+            operation_status[index]=['C' for _ in range(len(student_op_nodes))]
+            # fact_status 共有两种 known/unknown
+            student_fact_status[index]=['unknown' for _ in range(len(student_fact_nodes))]   
+    info=Info(student_fact_status=student_fact_status,
+              operation_right=operation_right,
+              operation_wrong=operation_wrong,
+              student_num=num_student,
+              fact_info=fact_info,
+              student_op_num=student_op_num,
+              student_fact_num=student_fact_num,
+              pair_info=pair_info,
+              student_status=student_status,
+              operation_status=operation_status
+              )
+    info.save_as_json(store_path)
 
-def auto_judge(review_id,update_data,json_dic_path,store_path,same_in_df_right_path,from_student_id_list_path):
+## DONE
+def auto_judge(review_id,update_data,json_dic_path,store_path,same_in_df_right_path):
     ## review_id为批改学生id
     ## 根据一次学生作业数据自动批改，返回下一个批改的学生
     refresh_student_data(review_id,update_data)
-    refresh_backend_data(json_dic_path,store_path,same_in_df_right_path,from_student_id_list_path)
+    refresh_backend_data(json_dic_path,store_path,same_in_df_right_path)
     if is_finished(store_path):
         return "Already finish"
     else:
         return select_student()
+
+if __name__ == "__main__":
+    json_dic_path = '/home/user/shared/solving_data/student_graph3/'
+    store_path = 'store.json'
+    fact_list_path = '/home/user/shared/solving_data/student_graph3/facts_list_v4.txt'
+    same_in_df_right_path = 'data/same_in_df_right.csv'
+    judge_init(json_dic_path,store_path,fact_list_path,same_in_df_right_path)
+    
